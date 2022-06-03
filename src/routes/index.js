8e1594bc73557fb7be0c692495674210e247a649
PQtai <phamquoctai09082001@gmail.com>
2022-06-04 01:20:55 +0700

    fix url deploy react

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import {Home , Search} from '../pages'
 import SidebarLayout from '../components/Layout/SidebarLayout'
 const publicRoutes = [
-    {path: '/',component: Home},
-    {path: '/search',component: Search , layout : SidebarLayout},
+    {path: 'HONGO_QTAI-Commercejs/',component: Home},
+    {path: 'HONGO_QTAI-Commercejs/search',component: Search , layout : SidebarLayout},
 ]
 const privateRoutes = []
 
