91ddfb5997c22c13641644c98f57a17b5ea95ff8
phamquoctai <phamquoctai09082001@gmail.com>
2022-07-11 20:42:17 +0700

    fix deploy git and hide file .env

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js'
 
-export const commerce = new Commerce('pk_41996763d5fd6d941042b39f3e420998e7fb78d2675e2', true);
+export const commerce = new Commerce(process.env.REACT_APP_CHECK_PUBLIC_KEY, true);
